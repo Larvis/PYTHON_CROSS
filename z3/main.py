4e9d039b58d0d1936883824873cdcc86c147467b
Larvis <larvis@inbox.ru>
2022-10-18 15:57:06 +0600

    Z3 - completed

--- z3/main.py
+++ z3/main.py
@@ -33,16 +33,31 @@ def counting(array):
     # как начальное значение для макс/мин берется первый элемент массива
     max_value = array[0][0]
     min_value = array[0][0]
+    next_aft_max = min_value = array[0][0]
+
     for i in range(len(array)):
         for j in range(len(array[i])):
             e = array[i][j]
+
             if e > max_value:
                 max_value = e
+                ## Если конец строки - то след. строка
+                if (len(array[i]) - 1) == j: # если последний элемент
+                    if (len(array) - 1) == i:  # если последний элемент в последней строке
+                        next_aft_max = array[0][0]
+                    else:  ## Если элемент последний, но строка не последняя
+                        next_aft_max = array[i + 1][0]
+                else: ## Если не последний элемент, то + 1
+                    next_aft_max = array[i][j + 1]
+                # print(i,j)
+                # print(next_aft_max)
+
+            ## Если конец столбца и строки - 0 0
             if e < min_value:
                 min_value = e
-    print("Максимум: %d, минимум: %d" % (max_value, min_value))
+    print("Максимум: %d, минимум: %d, след. после макс:%d" % (max_value, min_value, next_aft_max))
     print()
-    return max_value, min_value
+    return max_value, min_value, next_aft_max
 
 
 def main():
@@ -54,45 +69,40 @@ def main():
           "Если сумма наибольшего и наименьшего чисел больше нуля,\n"
           "то увеличить максимальный элемент в два раза,\n"
           "а наименьший уменьшить в два раза")
+    print('========================================================')
     # вызов функции вывода массива
     print_array(array)
     # вызов функции массива по условию, который возвращает элементы для проверки условия
-    max_value, min_value = counting(array)
+    max_value, min_value, next_aft_max = counting(array)
     while True:
         print("1. Заполнить массив случайными числами;")
         print("2. Выполнить задание;")
         print("3. Выход.")
         key = input('Введите команду (1, 2 или 3): ')
+        print('========================================================')
         if key == '1':  # рандом, вывод и новые значения по условию нового массива
             array = random_array(rowCount, colCount)
             print_array(array)
-            max_value, min_value = counting(array)
+            max_value, min_value, next_aft_max = counting(array)
         elif key == '2':
             # проверка выполнения условия
-            if (max_value + min_value) <= 0:
-                print("Сумма наибольшего и наименьшего чисел (%d) не больше нуля " % (max_value + min_value))
-                print("Задание не будет выполнено.")
+            if next_aft_max == 0:
+                print("Следующее число после (%d) равно нулю (%d)" %(max_value, next_aft_max))
+                print("Условия не выполнены.")
+                print('========================================================')
             else:
                 # выполнения результата совпадения условия,
                 # в данном случае макс * 2, а мин / 2
-                print("Cумма наибольшего и наименьшего чисел больше нуля (%d)" % (max_value + min_value))
+                print('========================================================')
+                print("Следующее число после (%d) не равно нулю (%d)" % (max_value, next_aft_max))
+                print("Условие выполнено, все положительные значения будут увеличены в 2 раза.")
+                print('========================================================')
                 for i in range(len(array)):  # перебор каждую строку
-                    while True:  # цикл нужен, так как макс/мин не одни в массиве
-                        try:
-                            index_max = array[i].index(max_value)  # положение макс в строке
-                            array[i][index_max] *= 2
-                        except ValueError:
-                            break
-                for i in range(len(array)):
-                    while True:
-                        try:
-                            index_min = array[i].index(min_value)  # положение мин в строке
-                            array[i][index_min] /= 2
-                        except ValueError:
-                            break
+                    for j in range(len(array[i])):  # перебор каждую строку
+                        if array[i][j] > 0:
+                            array[i][j] *= 2
 
-                print("Макс. элемент таблицы (все, если их больше одного) был(и) увеличен(ы) в два раза.")
-                print("Мин. элемент таблицы (все, если их больше одного) был(и) уменьшен(ы) в два раза.")
+                print("Все положительные элементы таблицы был увеличены в два раза.")
                 print_array(array)
                 break  # выход из цикла
         elif key == '3':
